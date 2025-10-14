74d4d33bbe45d2977e11b85a8d5e1425d629c22a
koooolaaaa <125712030+koooolaaaa@users.noreply.github.com>
2025-10-14 15:04:26 -0700

    remove unnecessary import

--- app/src/main/java/com/example/dyk_kyong_do_stressmeter/domain/StressScorer.kt
+++ app/src/main/java/com/example/dyk_kyong_do_stressmeter/domain/StressScorer.kt
@@ -1,7 +1,5 @@
 package com.example.dyk_kyong_do_stressmeter.domain
 
-import org.intellij.lang.annotations.JdkConstants
-
 object StressScorer {
     // 4x4 grid, positions 0..15 row-major
     private val map = listOf(5,8,14,16 ,5,7,13,15, 2,4,10,12, 1,3,9,11)

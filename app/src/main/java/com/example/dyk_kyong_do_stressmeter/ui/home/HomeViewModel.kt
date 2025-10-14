565524ea9ec3b1182c4b76058adfba1a3c2fe54a
koooolaaaa <125712030+koooolaaaa@users.noreply.github.com>
2025-10-14 14:03:29 -0700

    initialize the project

---
+++ app/src/main/java/com/example/dyk_kyong_do_stressmeter/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.dyk_kyong_do_stressmeter.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
